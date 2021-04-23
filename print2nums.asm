64107e9a5d47bf309e65ff3d5a0ffddb0c7ff819
Yanivbirkan <yaniv.birkan@gmail.com>
2021-04-23 13:15:03 +0300

    get 2 nums and print the biger

---
+++ print2nums.asm
@@ -0,0 +1,125 @@
+IDEAL
+MODEL small  
+STACK 100h
+P186 
+
+DATASEG
+num1 	db ?
+num2 	db ?
+smaller db ?
+bigger 	db ?
+ 
+CODESEG
+
+start:
+	mov ax, @data
+	mov ds, ax 
+	
+print_bigger:	
+	
+	; read first digit
+	mov ah,1
+	int 21h
+	sub al,'0'
+	mov [num1],al
+	
+	; read second digit
+	mov ah,1
+	int 21h
+	sub al,'0'
+	mov [num2],al
+
+	
+	call calc
+	
+skip_1:
+
+	; now we will print the result   
+	; print = 
+	mov dl,'='
+	mov ah,2
+	int 21h
+
+	; print bigger
+	mov dl,[bigger]
+	add dl,'0'
+	mov ah,2
+	int 21h
+	
+	mov cl ,[bigger]
+	call printcoc
+	
+	; new line
+	mov dl,10
+	mov ah,2
+	int 21h
+
+	; check if bigger is zero
+	cmp [bigger],0
+	jne print_bigger
+	
+	
+	
+exit:
+
+	mov ax, 4c00h
+	int 21h
+
+
+;====================================================================
+;   name  –  calc(calculate smaller and bigger)
+;   IN:  AL - num1/num2 , המספרים יועברו לפרוצדורה ברגיסטרים
+;   OUT:  הפרוצדורה תעדכן את המשתנים BIGGER ו SMALLER
+;	EFFECTED REGISTERS : al,ah,dl,cl   , 
+; ====================================================================
+proc calc
+pusha
+
+;calculate smaller and bigger
+	cmp al,[num1]
+	ja al_bigger
+	; al hold smaller
+	mov [smaller],al
+	mov ah,[num1]
+	mov [bigger],ah 
+	jmp skip_1
+al_bigger:
+	mov [bigger],al
+	mov ah,[num1]
+	mov [smaller],ah 
+
+
+	popa
+	ret
+endp
+
+
+
+
+
+;====================================================================
+;   name  –  printcoc(הפרוצדורה תדפיס כוכביות)
+;   IN:  CL - מספר הכוכבית
+;   OUT:   יודפסו כוכביות לפי מספר הכוכביות שקיבלנו בCL
+;	EFFECTED REGISTERS : dl,  
+; ===================================================================
+
+proc printcoc
+pusha
+	jcxz after
+	
+loop2:	
+	;הדפסה של כוכבית
+	MOV DL, '*' 
+	MOV AH,02h
+	INT 21H
+	
+	;לולאה
+	loop loop2
+	
+after:	
+	popa
+	ret
+endp
+
+END start
